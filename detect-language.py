3acea3cb667dbbbe32addf1506c93206704f5b4d
ThomasFaria <thomas.faria@ensae.fr>
2024-09-17 15:40:39 +0000

    use cuda if available

--- detect-language.py
+++ detect-language.py
@@ -3,16 +3,18 @@ import pandas as pd
 from transformers import pipeline
 import pyarrow.parquet as pq
 import pyarrow as pa
+import torch
 
 MODEL_NAME = "papluca/xlm-roberta-base-language-detection"
 URL_OUT = "s3://projet-dedup-oja/challenge_classification/raw-data/wi_dataset.parquet"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 fs = get_file_system()
 
 with fs.open("s3://projet-dedup-oja/challenge_classification/raw-data/wi_dataset.csv") as f:
     data = pd.read_csv(f, dtype=str)
 
-pipe = pipeline("text-classification", model=MODEL_NAME)
+pipe = pipeline("text-classification", model=MODEL_NAME, device=device)
 
 text = data.loc[:, "description"].to_list()
 
