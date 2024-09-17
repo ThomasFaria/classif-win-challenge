c98f9efbf7ec4241d3b75879b972b3a60f31de1c
ThomasFaria <thomas.faria@ensae.fr>
2024-09-17 15:02:01 +0000

    add python files

---
+++ detect-language.py
@@ -0,0 +1,27 @@
+from src.utils.data import get_file_system
+import pandas as pd
+from transformers import pipeline
+import pyarrow.parquet as pq
+import pyarrow as pa
+
+MODEL_NAME = "papluca/xlm-roberta-base-language-detection"
+URL_OUT = "s3://projet-dedup-oja/challenge_classification/raw-data/wi_dataset.parquet"
+
+fs = get_file_system()
+
+with fs.open("s3://projet-dedup-oja/challenge_classification/raw-data/wi_dataset.csv") as f:
+    data = pd.read_csv(f, dtype=str)
+
+pipe = pipeline("text-classification", model=MODEL_NAME)
+
+text = data.loc[:, "description"].to_list()
+
+results = pipe(text, top_k=1, truncation=True)
+df = data.merge(pd.DataFrame([d[0] for d in results]), left_index=True, right_index=True)
+df.set_index("id")
+
+pq.write_table(
+    pa.Table.from_pandas(df),
+    URL_OUT,
+    filesystem=fs,
+)
