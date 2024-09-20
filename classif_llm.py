61886f24afb54ede35d68222016f63563d18857a
ThomasFaria <thomas.faria@ensae.fr>
2024-09-20 09:57:12 +0000

    v0 classif llm

--- classif_llm.py
+++ classif_llm.py
@@ -1,38 +1,31 @@
 from src.utils.data import get_file_system
 from src.llm.build_llm import build_llm_model
+from src.vector_db.loaders import load_retriever
 import torch
+from src.utils.mapping import lang_mapping
 
 import os
 from src.prompting.prompts import RAG_PROMPT_TEMPLATE, format_docs
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import PydanticOutputParser  # , StrOutputParser
 import pyarrow.dataset as ds
 
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
-
+from tqdm import tqdm
+from src.response.response_llm import LLMResponse, ISCO_CODES
+import pandas as pd
+import pyarrow.parquet as pq
+import pyarrow as pa
 
 EMBEDDING_MODEL = "OrdalieTech/Solon-embeddings-large-0.1"
-URL_IN = "s3://projet-dedup-oja/challenge_classification/raw-data/wi_labels.csv"
+URL_IN = "projet-dedup-oja/challenge_classification/processed-data/wi_dataset_by_lang_translated/"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 CHROMA_DB_LOCAL_DIRECTORY = "data/chroma_db"
 COLLECTION_NAME = "test"
 LLM_MODEL = "mistralai/Mistral-7B-Instruct-v0.3"
-
-
+MAX_CODE_RETRIEVED = 30
+SEARCH_ALGO = "similarity"
 fs = get_file_system()
-
-data = (
-    ds.dataset(
-        URL_IN,
-        partitioning=["lang"],
-        format="parquet",
-        filesystem=fs,
-    )
-    .to_table()
-    .filter((ds.field("lang") == "lang=fr"))
-    .to_pandas()
-)
+URL_OUT = "s3://projet-dedup-oja/challenge_classification/processed-data/predictions_by_lang"
 
 llm, tokenizer = build_llm_model(
     model_name=LLM_MODEL,
@@ -42,54 +35,108 @@ llm, tokenizer = build_llm_model(
 )
 
 
-emb_model = HuggingFaceEmbeddings(
-    model_name=EMBEDDING_MODEL,
-    multi_process=False,
-    model_kwargs={"device": device, "trust_remote_code": True},
-    encode_kwargs={"normalize_embeddings": True},
-    show_progress=True,
-)
-db = Chroma(
+retriever = load_retriever(
+    embedding_model=EMBEDDING_MODEL,
     collection_name=COLLECTION_NAME,
-    persist_directory=CHROMA_DB_LOCAL_DIRECTORY,
-    embedding_function=emb_model,
+    db_directory=CHROMA_DB_LOCAL_DIRECTORY,
+    search_algo=SEARCH_ALGO,
+    device=device,
+    max_code_retrieved=MAX_CODE_RETRIEVED,
 )
 
 
-retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 30})
-
-description = data.loc[0, "description"]
-title = data.loc[0, "title"]
-
-# Ici faut voir ce qu'on fait mais faut virer/nettoyer/selectionner les documents
-retrieved_docs = retriever.invoke(" ".join([title, description]))
-retrieved_docs_unique = []
-for item in retrieved_docs:
-    if item not in retrieved_docs_unique:
-        retrieved_docs_unique.append(item)
-
-prompt_template = PromptTemplate.from_template(
-    template=RAG_PROMPT_TEMPLATE,
-)
-
-# TODO: make sure the prompt is not to long
-prompt = prompt_template.format(
-    **{
-        "title": title,
-        "description": description,
-        "proposed_categories": format_docs(retrieved_docs_unique),
-    }
-)
-
-# On crée la chaine de traitement
-chain = prompt_template | llm | StrOutputParser()
+with fs.open("s3://projet-dedup-oja/challenge_classification/raw-data/wi_labels.csv") as f:
+    labels = pd.read_csv(f, dtype=str)
 
 
-# On interroge le LLM
-chain.invoke(
-    {
-        "title": title,
-        "description": description,
-        "proposed_categories": format_docs(retrieved_docs_unique),
-    },
-)
+for lang in lang_mapping.lang_iso_2:
+    print(f"Prediction for language: {lang}")
+    data = (
+        ds.dataset(
+            URL_IN,
+            partitioning=["lang"],
+            format="parquet",
+            filesystem=fs,
+        )
+        .to_table()
+        .filter((ds.field("lang") == f"lang={lang}"))
+        .to_pandas()
+    )
+
+    if data.empty:
+        continue
+
+    # Reformat partionnning column
+    data.loc[:, "lang"] = data.loc[:, "lang"].str.replace("lang=", "")
+
+    data_sub = data.head(10)
+    results = []
+
+    for row in tqdm(data_sub.itertuples(), total=data_sub.shape[0]):
+        description = row.translation
+        title = row.title
+        id = row.id
+
+        # Ici faut voir ce qu'on fait mais faut virer/nettoyer/selectionner les documents
+        retrieved_docs = retriever.invoke(" ".join([title, description]))
+        retrieved_docs_unique = []
+        for item in retrieved_docs:
+            if item not in retrieved_docs_unique:
+                retrieved_docs_unique.append(item)
+
+        parser = PydanticOutputParser(pydantic_object=LLMResponse)
+        prompt_template = PromptTemplate.from_template(
+            template=RAG_PROMPT_TEMPLATE,
+            partial_variables={"format_instructions": parser.get_format_instructions()},
+        )
+
+        # TODO: make sure the prompt is not to long
+        # prompt = prompt_template.format(
+        #     **{
+        #         "title": title,
+        #         "description": description,
+        #         "proposed_categories": format_docs(retrieved_docs_unique),
+        #     }
+        # )
+
+        # On crée la chaine de traitement
+        chain = prompt_template | llm | parser
+
+        # On interroge le LLM
+        try:
+            response = chain.invoke(
+                {
+                    "title": title,
+                    "description": description,
+                    "proposed_categories": format_docs(retrieved_docs_unique),
+                },
+            )
+        except ValueError as parse_error:
+            print(f"Unable to parse llm response: {str(parse_error)}")
+            # logger.error(f"Unable to parse llm response: {str(parse_error)}")
+
+            response = LLMResponse(
+                codable=False,
+            )
+
+        # Make sure the code is part of the list of ISCO codes
+        if response.class_code not in ISCO_CODES:
+            response.codable = False
+            response.class_code = None
+            response.likelihood = None
+            label_code = None
+        else:
+            label_code = labels.loc[labels["code"] == response.class_code, "label"].values[0]
+
+        results.append(response.dict() | {"id": id, "label_code": label_code})
+
+    predictions = data_sub.merge(pd.DataFrame(results), on="id")
+
+    pq.write_to_dataset(
+        pa.Table.from_pandas(predictions),
+        root_path=URL_OUT,
+        partition_cols=["lang"],
+        basename_template="part-{i}.parquet",
+        existing_data_behavior="overwrite_or_ignore",
+        filesystem=fs,
+    )
